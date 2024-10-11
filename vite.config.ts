2fba4259effe30c24620c6c689e68783a5f39709
snailuu <huangzhuofeng@idea.edu.cn>
2024-10-11 10:57:07 +0800

    feat: .gz压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,45 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import path, { resolve } from 'path';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve('./src'),
+    },
+  },
+  build: {
+    outDir: resolve(__dirname, 'dist'), // 指定输出路径
+    chunkSizeWarningLimit: 1500,
+    sourcemap: false, // 是否生成 source map
+    emptyOutDir: true, //Vite 会在构建时清空该目录
+    // 打包时清楚console和debugger
+    terserOptions: {
+      compress: {
+        keep_infinity: true,
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        compact: true, //压缩代码，删除换行符等
+        assetFileNames: '[ext]/[name]-[hash].[ext]', //静态文件输出的文件夹名称
+        chunkFileNames: 'js/[name]-[hash].js', //chunk包输出的文件夹名称
+        entryFileNames: 'js/[name]-[hash].js', //入口文件输出的文件夹名称
+      },
     },
   },
 });
